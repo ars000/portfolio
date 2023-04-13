4d5d6f010e75cd287a0dbc125298f39aefab0c5f
ars000 <ars000@rambler.ru>
2023-04-13 16:19:52 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -89,3 +89,62 @@ $(function () {
   }, 100)
 });
 
+
+// Coding animation
+const appContentTl = gsap.timeline({
+  repeat: -1,
+});
+
+appContentTl.to('#app__content__top, #app__content__mid, #app__content__bot', {x: 0, opacity: 0});
+appContentTl.fromTo('#app__content__top', {x: -10, opacity: 0, duration: 1}, {x: 0, opacity: 1});
+appContentTl.fromTo('#app__content__mid', {x: -10, opacity: 0, duration: 1}, {x: 0, opacity: 1}, '<0.5');
+appContentTl.fromTo('#app__content__bot', {x: -10, opacity: 0, duration: 1}, {x: 0, opacity: 1}, '<0.75');
+appContentTl.to('#app__content__top, #app__content__mid, #app__content__bot', {x: 0, opacity: 0, duration: 0.5,}, '<7');
+
+
+const buttonsTl = gsap.timeline({
+  repeat: -1,
+  yoyo: true,
+  delay: 7
+});
+buttonsTl.fromTo('.coding-anim__button', {y: 0, duration: 2.5},  {y: -10, duration: 2.5});
+
+const ideaTl = gsap.timeline({
+  repeat: -1,
+});
+ideaTl.to('#idea', {opacity: 0, duration: 0.5});
+ideaTl.fromTo('#idea', {scale: 0, opacity: 0, transformOrigin: '50% 50%'}, {scale: 1, opacity: 1, transformOrigin: '50% 50%', ease: "back.out(1.7)"}, '+=5');
+ideaTl.to('#idea', {opacity: 0, duration: 1}, '+=15');
+
+const mainTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '#about',
+    start: '20% 75%',
+    toggleActions: 'restart none play none'
+  },
+  
+  onUpdate: restart,
+  onComplete: restart
+});
+mainTl.to('#app__content__top, #app__content__mid, #app__content__bot', {x: 0, opacity: 0});
+mainTl.to('#idea', {opacity: 0});
+mainTl.from('#hand-coding', {y: -100, opacity: 0, duration: 1.2});
+mainTl.to('#app', {x: -13, y: -54, opacity: 0, duration: 0}, '<-1.2');
+mainTl.from('#desk__shadow', {scale: 1.2, opacity: 0, transformOrigin: '50% 50%', duration: .6},'-=0.5');
+mainTl.from('#laptop', {y: -50, duration: 1.2},'-=1.2');
+mainTl.from('#laptop__shadow', {scale: 1.1, opacity: 0, transformOrigin: '50% 50%', duration: 1},'<0.6');
+mainTl.from('#notebook', {y: -50, opacity: 0, duration: 1},'<-=1');
+mainTl.from('#notebook__shadow', {scale: 1.2, opacity: 0, transformOrigin: '50% 50%', duration: 1},'>-=0.6');
+mainTl.fromTo('#app', {x: -13, y: -54, opacity: 0, duration: 1.2, }, {x: -10, y: -4, opacity: 1, duration: 1.2}, '<');
+mainTl.fromTo('#app', {x: -10, y: -4, duration: 1.2, }, {x: 0, y: 0, duration: 1.2}, '<1.2');
+mainTl.from('.coding-anim__button', {y: -70, opacity: 0, duration: 1.2, stagger: -0.1}, '<-=1.5');
+mainTl.from('#coffee', {y: -70, opacity: 0, duration: 1.2}, '<1');
+mainTl.from('#coffee__shadow', {scale: 1.2, opacity: 0, transformOrigin: '50% 50%', duration: .6}, '<0.8');
+mainTl.from('#man, #armchair', {x: 40, opacity: 0, duration: 1}, '<-0.8');
+mainTl.from('#app__content__left, #app__content__right', {x: -10, opacity: 0, duration: 1}, '+=0.3');
+
+function restart() {
+  appContentTl.restart(true, false);
+  buttonsTl.restart(true, false);
+  ideaTl.restart(true, false);
+}
